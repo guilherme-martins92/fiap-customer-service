e3cf918e36cb3e03e583b458ce8fab756aeef148
Guilherme Felipe Martins <guilherme.martins92@outlook.com>
2025-07-16 21:51:51 -0300

    Enhance GetCustomerByIdUseCase for DTO and encryption
    - Added using directives for DTOs and mappings.
    - Introduced ISensitiveDataEncryptor to handle sensitive data.
    - Updated constructor to accept ISensitiveDataEncryptor.
    - Changed ExecuteAsync return type to Result<CustomerOutputDto>.
    - Implemented decryption of sensitive data in ExecuteAsync.
    - Updated success and failure return statements to use CustomerMapper.
    - Overall improvements for better data representation and handling.

--- src/Fiap.CustomerService.Application/UseCases/GetCustomerByIdUseCase/GetCustomerByIdUseCase.cs
+++ src/Fiap.CustomerService.Application/UseCases/GetCustomerByIdUseCase/GetCustomerByIdUseCase.cs
@@ -1,4 +1,6 @@
 ﻿using Fiap.CustomerService.Application.Common;
+using Fiap.CustomerService.Application.DTOs;
+using Fiap.CustomerService.Application.Mappings;
 using Fiap.CustomerService.Domain.Interfaces;
 using Microsoft.Extensions.Logging;
 
@@ -8,36 +10,41 @@ namespace Fiap.CustomerService.Application.UseCases.GetCustomerByIdUseCase
     {
         private readonly ICustomerRepository _customerRepository;
         private readonly ILogger<GetCustomerByIdUseCase> _logger;
-        public GetCustomerByIdUseCase(ICustomerRepository customerRepository, ILogger<GetCustomerByIdUseCase> logger)
+        private readonly ISensitiveDataEncryptor _sensitiveDataDecryptor;
+        public GetCustomerByIdUseCase(ICustomerRepository customerRepository, ILogger<GetCustomerByIdUseCase> logger, ISensitiveDataEncryptor sensitiveDataDecryptor)
         {
             _customerRepository = customerRepository;
             _logger = logger;
+            _sensitiveDataDecryptor = sensitiveDataDecryptor;
         }
 
-        public async Task<Result<GetCustomerByIdOutput>> ExecuteAsync(Guid id)
+        public async Task<Result<CustomerOutputDto>> ExecuteAsync(Guid id)
         {
             try
             {
                 if (id == Guid.Empty)
                 {
                     _logger.LogError("Invalid customer ID: {Id}", id);
-                    return Result<GetCustomerByIdOutput>.Failure(new List<string> { "Invalid customer ID." });
+                    return Result<CustomerOutputDto>.Failure(new List<string> { "Invalid customer ID." });
                 }
 
                 var customer = await _customerRepository.GetByIdAsync(id);
+
                 if (customer == null)
                 {
                     _logger.LogWarning("Customer not found with ID: {Id}", id);
-                    return Result<GetCustomerByIdOutput>.Failure(new List<string> { "Customer not found." });
+                    return Result<CustomerOutputDto>.Failure(new List<string> { "Customer not found." });
                 }
 
+                customer = await _sensitiveDataDecryptor.DecryptcustomerAsync(customer);
+
                 _logger.LogInformation("Customer retrieved successfully with ID: {Id}", id);
-                return Result<GetCustomerByIdOutput>.Success(GetCustomerByIdOutput.FromEntity(customer));
+                return Result<CustomerOutputDto>.Success(CustomerMapper.FromEntity(customer));
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "An error occurred while retrieving customer with ID: {Id}", id);
-                return Result<GetCustomerByIdOutput>.Failure(new List<string> { "An error occurred while retrieving the customer." });
+                return Result<CustomerOutputDto>.Failure(new List<string> { "An error occurred while retrieving the customer." });
             }
         }
     }
